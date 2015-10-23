557999be8ac02c7d04b3d25fd677d446fedb2c7f
thuanbc <bcthuan07@gmail.com>
2015-10-23 12:07:10 +0700

    change gradle's configure

--- src/main/java/com/example/StarterApplication.java
+++ src/main/java/com/example/StarterApplication.java
@@ -2,11 +2,18 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StarterApplication {
+public class StarterApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(StarterApplication.class, args);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(StarterApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterApplication.class, args);
+	}
 }
